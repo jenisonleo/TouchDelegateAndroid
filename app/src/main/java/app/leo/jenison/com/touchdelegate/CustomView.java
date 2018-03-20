6ef4e684e5953ed1a45d045daa33facecac85347
jenisonleo <jenisongracious@gmail.com>
2018-03-20 15:06:58 +0530

    initial push

--- app/src/main/java/app/leo/jenison/com/touchdelegate/CustomView.java
+++ app/src/main/java/app/leo/jenison/com/touchdelegate/CustomView.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by jenison-3631 on 19/03/18.
@@ -16,6 +17,7 @@ public class CustomView extends View {
         super(context);
         intt();
     }
+    private Toast toast;
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -30,6 +32,11 @@ public class CustomView extends View {
         setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                if(toast!=null){
+                    toast.cancel();
+                }
+                toast=Toast.makeText(getContext()," "+event.getX()+" "+event.getY(),Toast.LENGTH_SHORT);
+                toast.show();
                 Log.e("child "," "+event.getX()+" "+event.getY());
                 return true;
             }
